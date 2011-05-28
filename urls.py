bd4305437bb76f1f7ffe9134d08a7c5490a2e4cd
Seth Chisamore <schisamo@opscode.com>
2011-05-27 21:47:56 -0400

    allow static files to be served out of MEDIA_ROOT

--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
+    url(r'^(?P<path>.*)', 'django.views.static.serve', { 'document_root' : settings.MEDIA_ROOT }),
 )
 
 if settings.DEBUG:
